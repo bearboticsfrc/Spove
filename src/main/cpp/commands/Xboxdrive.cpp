fe55e17b5fce7c96fd0e7d27d8838539d881ceb9
troy707 <troy7199@gmail.com>
2021-01-30 17:43:02 -0700

    Added XboxController(needs turning)

---
+++ src/main/cpp/commands/Xboxdrive.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/Xboxdrive.h"
+
+using JoystickHand = frc::GenericHID::JoystickHand;
+
+Xboxdrive::Xboxdrive(frc::XboxController *xboxcontroller, Drivetrain *drivetrain)
+: m_xboxcontroller{xboxcontroller},m_drivetrain{drivetrain}  
+{
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements(m_drivetrain);
+}
+
+// Called when the command is initially scheduled.
+void Xboxdrive::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void Xboxdrive::Execute() {
+    double speed = m_xboxcontroller->GetY(JoystickHand::kLeftHand);
+    double turn = m_xboxcontroller->GetX(JoystickHand::kLeftHand);
+    m_drivetrain->drive(speed + turn,speed - turn);
+    }
+
+// Called once the command ends or is interrupted.
+void Xboxdrive::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool Xboxdrive::IsFinished() {
+  return false;
+}
